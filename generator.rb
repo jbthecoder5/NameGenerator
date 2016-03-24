51ae229173bb0037baa29ee14d5c727bdfe4c38d
Joseph P Bourne <kiratsuchi@live.com>
2016-03-23 19:28:52 -0700

    username will be automated, without prompting user.

--- generator.rb
+++ generator.rb
@@ -2,35 +2,27 @@ class Generator
 
 	def initialize
 		@alph = "abcdefghijklmnopqrstuvwxyz"
-		@letter = ""
+		@randomName = ""
 
-		print "\n\nDo you wish to generate random name?"
-		print "\nEnter '1' to continue, or '0' to quit\n\n"
 
-		@uc = gets.chomp.to_i
-
-		if(@uc == 1)
-			6.times do |str|
-				str = @alph[rand(0..25)]
-				@letter += str
-			end
-		else
-			return false
+		6.times do |str|
+			str = @alph[rand(0..25)]
+			@randomName += str
 		end
+		
 	end
 
 	def autoGen
-
-		return @letter
+		return @randomName
 	end
 
 	# return the auto-generated string in capital letters
 	def capGen
-		@letter.capitalize!
+		@randomName.capitalize!
 	end
 
 	# return the auto-generated string in lowercase
 	def lowGen
-		@letter.lowercase!
+		@randomName.lowercase!
 	end
 end
